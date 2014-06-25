55ec0043561d713e8d3a6858b76b6d32d878a9cc
Thomas Coler <thomas@valueoutlook.com>
2014-06-25 06:28:30 -0600

    First commit

---
+++ app/models/account.rb
@@ -0,0 +1,14 @@
+class Account < ActiveRecord::Base
+	has_many :account_entries
+
+	validates :name, presence: true, length: {in: 1..70, message: "Please use a decent name, sir."}, uniqueness: true
+
+	validate :your_name_is_not_dumb
+
+
+	def your_name_is_not_dumb
+		if name.include?("dumb")
+			errors.add(:name, "is dumb!")
+		end
+	end
+end
---
+++ app/models/customer.rb
@@ -0,0 +1,4 @@
+class Customer < Account
+	has_many :time_entries
+end
+
